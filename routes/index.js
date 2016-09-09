270b2c6d05a56ee9ca49063e7e707c7ddfca022c
Jacob Madden <jacob.madden@gmail.com>
2016-09-08 21:56:00 -0400

    added facebook messenger webook

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// for Facebook verification
+router.get('/webhook/', function (req, res) {
+    if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+        res.send(req.query['hub.challenge'])
+    }
+    res.send('Error, wrong token')
+})
+
 module.exports = router;
